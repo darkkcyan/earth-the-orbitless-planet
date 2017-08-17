9b69672a064c3d2a5e38e7d4d0c8ff83fc41305d
Tran Quang Loc <phongcachhiphop@gmail.com>
2017-08-18 00:03:03 +0700

    add rerender function

--- tools/planet-surface-maker/main.ts
+++ tools/planet-surface-maker/main.ts
@@ -1,18 +1,31 @@
 const imageInput = document.getElementById("image-input") as HTMLInputElement;
-const image = document.getElementById("image-file") as HTMLImageElement;
+const sourceImage = document.getElementById("image-file") as HTMLImageElement;
+
 const widthInput = document.getElementById("width") as HTMLInputElement;
 const heightInput = document.getElementById("height") as HTMLInputElement;
+
+const editorCanvas = document.getElementById("editor") as HTMLCanvasElement;
+
+function rerender() {
+  const ectx = editorCanvas.getContext("2d");
+  const w = +widthInput.value;
+  const h = +heightInput.value;
+  ectx.clearRect(0, 0, w, h);
+  ectx.drawImage(sourceImage, 0, 0, w, h);
+}
 
 imageInput.onchange = (e: Event) => {
   const file = imageInput.files[0];
   const reader = new FileReader();
   reader.onload = () => {
-    image.src = reader.result;
+    sourceImage.src = reader.result;
   };
   if (file) {
     reader.readAsDataURL(file);
   }
 };
+
+sourceImage.onload = rerender;
 
 function onChangeSize() {
   const list = document.getElementsByTagName("canvas");
@@ -22,6 +35,7 @@ function onChangeSize() {
     list[i].height = +heightInput.value;
   }
   /* tslint:enable prefer-for-of */
+  rerender();
 }
 
 widthInput.onchange = heightInput.onchange = onChangeSize;
