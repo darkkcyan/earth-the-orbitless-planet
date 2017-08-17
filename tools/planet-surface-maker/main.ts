0fa7f82cb1242e1d1218f7620adcbd6be04846ed
Tran Quang Loc <phongcachhiphop@gmail.com>
2017-08-17 23:56:25 +0700

    add events for size input

--- tools/planet-surface-maker/main.ts
+++ tools/planet-surface-maker/main.ts
@@ -1,5 +1,7 @@
 const imageInput = document.getElementById("image-input") as HTMLInputElement;
 const image = document.getElementById("image-file") as HTMLImageElement;
+const widthInput = document.getElementById("width") as HTMLInputElement;
+const heightInput = document.getElementById("height") as HTMLInputElement;
 
 imageInput.onchange = (e: Event) => {
   const file = imageInput.files[0];
@@ -10,4 +12,21 @@ imageInput.onchange = (e: Event) => {
   if (file) {
     reader.readAsDataURL(file);
   }
+};
+
+function onChangeSize() {
+  const list = document.getElementsByTagName("canvas");
+  /* tslint:disable prefer-for-of */
+  for (let i = 0; i < list.length; ++i) {
+    list[i].width = +widthInput.value;
+    list[i].height = +heightInput.value;
+  }
+  /* tslint:enable prefer-for-of */
+}
+
+widthInput.onchange = heightInput.onchange = onChangeSize;
+
+// main
+document.onreadystatechange = () => {
+  onChangeSize();
 };
