f9e4ef462d4d956a50049edaea66c94f6300490b
Tran Quang Loc <phongcachhiphop@gmail.com>
2017-08-19 23:07:59 +0700

    add flame to rocket

--- src/Player.ts
+++ src/Player.ts
@@ -8,8 +8,11 @@ export class Rocket {
   public movingDuration = 10;
 
   private movingTime = 0;
+  private previousX: number = 0;
+  private previousY: number = 0;
+  private realFlameSize: number;
 
-  constructor(public rocketSize: number, public movingRange: number) {
+  constructor(public rocketSize: number, public flameSize: number, public movingRange: number) {
   }
 
   public process(dt: number) {
@@ -17,25 +20,64 @@ export class Rocket {
     if (this.movingTime > this.movingDuration) {
       this.movingTime -= this.movingDuration;
     }
+    const RANDOM_RANGE = this.flameSize / 7;
+    this.realFlameSize = this.flameSize + (this.x - this.previousX) * 3;
+    this.realFlameSize += RANDOM_RANGE * (Math.random() - 0.5);
+    this.previousX = this.x;
+    this.previousY = this.y;
   }
 
   public renderRocketPart(ctx: CanvasRenderingContext2D) {
     ctx.fillStyle = "white";
-    const x = this.x - this.movingRange * Math.cos(PI2 * this.movingTime / this.movingDuration);
+    const x = this.x - this.getMovingOffset();
     const y = this.y;
-    const rs = this.rocketSize;
-    ctx.fillRect(x - rs, y - rs / 2, rs / 4, rs);
+    const halfrs = this.rocketSize / 2;
+    ctx.fillRect(x - halfrs * 2, y - halfrs, halfrs / 2, halfrs * 2);
     ctx.fillStyle = "black";
-    ctx.fillRect(x - rs * 3 / 4, y - rs / 2, rs / 4, rs);
+    ctx.fillRect(x - halfrs * 1.5, y - halfrs, halfrs / 2, halfrs * 2);
     ctx.fillStyle = "gray";
     ctx.beginPath();
-    ctx.arc(x - rs / 2, y, rs / 2, -HALF_PI, HALF_PI);
+    ctx.arc(x - halfrs, y, halfrs, -HALF_PI, HALF_PI);
     ctx.fill();
+  }
+
+  public renderFlamePart(ctx: CanvasRenderingContext2D) {
+    const halfrs = this.rocketSize / 2;
+    const x = this.x - this.getMovingOffset() - halfrs * 2;
+    const y = this.y;
+    const magic = 0.618;  // the magic is golden ratio
+    const magic2 = magic * magic;
+    ctx.save();
+    ctx.shadowBlur = 20;
+    ctx.globalAlpha = 0.5;
+    ctx.beginPath();
+    ctx.moveTo(x, y - halfrs * magic);
+    ctx.lineTo(x - this.realFlameSize * magic, y - halfrs);
+    ctx.lineTo(x - this.realFlameSize, y);
+    ctx.lineTo(x - this.realFlameSize * magic, y + halfrs);
+    ctx.lineTo(x, y + halfrs * magic);
+    ctx.shadowColor = ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(x, y - halfrs * magic2);
+    ctx.lineTo(x - this.realFlameSize * magic2, y - halfrs * magic);
+    ctx.lineTo(x - this.realFlameSize * magic, y);
+    ctx.lineTo(x - this.realFlameSize * magic2, y + halfrs * magic);
+    ctx.lineTo(x, y + halfrs * magic2);
+    ctx.shadowColor = ctx.fillStyle = "yellow";
+    ctx.fill();
+    ctx.restore();
   }
 
   public render(ctx: CanvasRenderingContext2D) {
     this.renderRocketPart(ctx);
+    this.renderFlamePart(ctx);
   }
+
+  private getMovingOffset() {
+    return this.movingRange * Math.cos(PI2 * this.movingTime / this.movingDuration);
+  }
+
 }
 
 export default class Player {
@@ -55,7 +97,7 @@ export default class Player {
   get y() { return this._y; }
 
   constructor(private planet: Planet) {
-    this.roc = new Rocket(this.planet.radius / 2.5, 10);
+    this.roc = new Rocket(this.planet.radius / 2.5, this.planet.radius / 1.5, 10);
   }
 
   public process(dt: number) {
