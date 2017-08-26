103fe387f9590352ff82c782e66b7068ad298661
Tran Quang Loc <phongcachhiphop@gmail.com>
2017-08-26 23:09:42 +0700

    merge 2 properties into one in ICollidable
    Also rename things :D.

--- src/SpatialHashMap.ts
+++ src/SpatialHashMap.ts
@@ -1,8 +1,13 @@
-import {collisionDetector, IShape, Rectangle} from "./shapes";
+import {
+  AllKindOfShapes,
+  isCollision,
+  IShape,
+  Rectangle,
+  SHAPE_TYPE,
+} from "./shapes";
 
 export interface ICollidable {
-  boundary: Rectangle;
-  shape: IShape;
+  collisionShape: AllKindOfShapes;
 }
 
 export default class SpatialHashMap {
@@ -11,39 +16,41 @@ export default class SpatialHashMap {
   }
 
   public insert(a: ICollidable) {
-    for (const p of this.getPositions(a.boundary)) {
-      const key = this.hashFunc(p);
-      if (!this.hash[key]) {
-        this.hash[key] = [];
+    const [sx, sy, ex, ey] = this.getPositions(a.collisionShape);
+    for (let i = sx; i <= ex; ++i) {
+      for (let f = sy; f <= ey; ++f) {
+        const key = this.hashFunc(i, f);
+        if (!this.hash[key]) {
+          this.hash[key] = [];
+        }
+        this.hash[key].push(a);
       }
-      this.hash[key].push(a);
     }
   }
 
   public retrive(a: ICollidable) {
     const ans: ICollidable[] = [];
-    const bpx = a.boundary.x >> this.powerOf2;
-    const bpy = a.boundary.y >> this.powerOf2;
-    for (const p of this.getPositions(a.boundary)) {
-      const key = this.hashFunc(p);
-      if (!this.hash[key]) {
-        continue;
-      }
-      for (const obj of this.hash[key]) {
-        if (obj === a) {
+    const [sx, sy, ex, ey] = this.getPositions(a.collisionShape);
+    for (let i = sx; i <= ex; ++i) {
+      for (let f = sy; f <= ey; ++f) {
+        const key = this.hashFunc(i, f);
+        if (!this.hash[key]) {
           continue;
         }
+        for (const obj of this.hash[key]) {
+          if (obj === a) {
+            continue;
+          }
 
-        // if this object is processed before
-        if (
-          Math.max(obj.boundary.x >> this.powerOf2, bpx) !== p[0] ||
-          Math.max(obj.boundary.y >> this.powerOf2, bpy) !== p[1]
-        ) {
-          continue;
-        }
+          // if this object is processed before
+          const [ox, oy] = this.getPositions(obj.collisionShape);
+          if (Math.max(ox, sx) !== i || Math.max(oy, sy) !== f) {
+            continue;
+          }
 
-        if (collisionDetector[obj.shape.shapeType][a.shape.shapeType](obj.shape, a.shape)) {
-          ans.push(obj);
+          if (isCollision(obj.collisionShape, a.collisionShape)) {
+            ans.push(obj);
+          }
         }
       }
     }
@@ -54,21 +61,15 @@ export default class SpatialHashMap {
     return [x >> this.powerOf2, y >> this.powerOf2];
   }
 
-  private getPositions(boundary: Rectangle) {
-    const sx = boundary.x >> this.powerOf2;
-    const sy = boundary.y >> this.powerOf2;
-    const ex = (boundary.x + boundary.width) >> this.powerOf2;
-    const ey = (boundary.y + boundary.height) >> this.powerOf2;
-    const ans: Array<[number, number]> = [];
-    for (let i = sx; i <= ey; ++i) {
-      for (let f = sy; f <= ey; ++f) {
-        ans.push([i, f]);
-      }
+  private getPositions(shape: AllKindOfShapes) {
+    const ans = shape.getBounds();
+    for (let i = ans.length; i--; ) {
+      ans[i] >>= this.powerOf2;
     }
     return ans;
   }
 
-  private hashFunc([x, y]: [number, number]) {
+  private hashFunc(x: number, y: number) {
     return (x << 25 | y);  // because the safe integer range is 2 ^ 53
   }
 }
--- src/shapes.ts
+++ src/shapes.ts
@@ -12,43 +12,46 @@ export class Rectangle implements IShape {
   public readonly shapeType = SHAPE_TYPE.RECTANGLE;
   constructor(public x: number, public y: number, public width: number, public height: number) {
   }
+  public getBounds() {
+    return [this.x, this.y, this.x + this.width, this.y + this.height];
+  }
 }
 
 export class Circle implements IShape {
   public readonly shapeType = SHAPE_TYPE.CIRCLE;
   constructor(public x: number, public y: number, public radius: number) {
   }
+  public getBounds() {
+    return [this.x - this.radius, this.y - this.radius, this.x + this.radius, this.y + this.radius];
+  }
 }
 
-type CollisionCheckFunction = (a: IShape, b: IShape) => boolean;
-export const collisionDetector: CollisionCheckFunction[][] = [[], []];
-
-collisionDetector[SHAPE_TYPE.RECTANGLE][SHAPE_TYPE.RECTANGLE] = (a: Rectangle, b: Rectangle) => {
-  return (
-    a.x < b.x + b.width &&
-    b.x < a.x + a.width &&
-    a.y < b.y + b.height &&
-    b.y < a.y + a.height
-  );
-};
-
-collisionDetector[SHAPE_TYPE.CIRCLE][SHAPE_TYPE.CIRCLE] = (a: Circle, b: Circle) => {
-  const tx = a.x - b.x;
-  const ty = a.y - b.y;
-  const r = a.radius + b.radius;
-  return tx * tx + ty * ty < r * r;
-};
+export type AllKindOfShapes = Rectangle | Circle;
 
 // circle and rectanble collision dectection algorithm get from here:
 // http://www.migapro.com/circle-and-rotated-rectangle-collision-detection/
-collisionDetector[SHAPE_TYPE.CIRCLE][SHAPE_TYPE.RECTANGLE] = (a: Circle, b: Rectangle) => {
-  const cx = clamp(a.x, b.x, b.x + b.width) - a.x;
-  const cy = clamp(a.y, b.y, b.y + b.height) - a.y;
-  return cx * cx + cy * cy < a.radius * a.radius;
-};
-
-collisionDetector[SHAPE_TYPE.RECTANGLE][SHAPE_TYPE.CIRCLE] = (b: Rectangle, a: Circle) => {
-  const cx = clamp(a.x, b.x, b.x + b.width) - a.x;
-  const cy = clamp(a.y, b.y, b.y + b.height) - a.y;
-  return cx * cx + cy * cy < a.radius * a.radius;
-};
+export function isCollision(a: AllKindOfShapes, b: AllKindOfShapes) {
+  if (a.shapeType === SHAPE_TYPE.RECTANGLE) {
+    if (b.shapeType === SHAPE_TYPE.RECTANGLE) {
+      return (
+        a.x < b.x + b.width &&
+        b.x < a.x + a.width &&
+        a.y < b.y + b.height &&
+        b.y < a.y + a.height
+      );
+    } else {
+      const cx = clamp(b.x, a.x, a.x + a.width) - b.x;
+      const cy = clamp(b.y, a.y, a.y + a.height) - b.y;
+      return cx * cx + cy * cy < b.radius * b.radius;
+    }
+  } else if (b.shapeType === SHAPE_TYPE.CIRCLE) {
+    const tx = a.x - b.x;
+    const ty = a.y - b.y;
+    const r = a.radius + b.radius;
+    return tx * tx + ty * ty < r * r;
+  } else {
+    const cx = clamp(a.x, b.x, b.x + b.width) - a.x;
+    const cy = clamp(a.y, b.y, b.y + b.height) - a.y;
+    return cx * cx + cy * cy < a.radius * a.radius;
+  }
+}
