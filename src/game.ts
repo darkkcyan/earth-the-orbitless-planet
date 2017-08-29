e38f2436279c106bd787dd4b540dcc34a0e9a7e5
Tran Quang Loc <phongcachhiphop@gmail.com>
2017-08-29 09:50:41 +0700

    add basic gameloop

---
+++ src/game.ts
@@ -0,0 +1,17 @@
+import ctx, {celm} from "./canvas";
+import {emit, Events} from "./EventListener";
+import Player from "./Player";
+
+export let player: Player = null;
+export let dt: number = null;
+
+let lastTime = Date.now();
+
+export function gameloop() {
+  dt = (Date.now() - lastTime) / 1000;
+  lastTime = Date.now();
+  celm.width ^= 0;
+  emit(Events.process, dt);
+  emit(Events.render, ctx);
+  requestAnimationFrame(gameloop);
+}
