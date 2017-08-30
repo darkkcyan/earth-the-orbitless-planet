63783b724b1d86f0f4a7fb00c3cdfa3982c2d1fd
Tran Quang Loc <phongcachhiphop@gmail.com>
2017-08-30 23:24:37 +0700

    add ease

---
+++ src/ease.ts
@@ -0,0 +1,16 @@
+// some functions get from here: http://gizma.com/easing/
+
+export function easeInOutCubic(t: number, b: number, c: number, d: number) {
+  t /= d / 2;
+  if (t < 1) {
+    return c / 2 * t * t * t + b;
+  }
+  t -= 2;
+  return c / 2 * (t * t * t + 2) + b;
+}
+
+export function easeOutCubic(t: number, b: number, c: number, d: number) {
+  t /= d;
+  t--;
+  return c * (t * t * t + 1) + b;
+}
