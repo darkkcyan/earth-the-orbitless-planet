a6198006edca2ebef084db78266f12b35563c95d
Tran Quang Loc <phongcachhiphop@gmail.com>
2017-08-30 23:53:10 +0700

    add more ease

--- src/ease.ts
+++ src/ease.ts
@@ -14,3 +14,12 @@ export function easeOutCubic(t: number, b: number, c: number, d: number) {
   t--;
   return c * (t * t * t + 1) + b;
 }
+
+export function easeInOutQuad(t: number, b: number, c: number, d: number) {
+  t /= d / 2;
+  if (t < 1) {
+    return c / 2 * t * t + b;
+  }
+  t--;
+  return -c / 2 * (t * (t - 2) - 1) + b;
+}
