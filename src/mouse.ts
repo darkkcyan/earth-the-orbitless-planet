c363ba5b7744d36564eedf8796ff00d1f73cca46
Tran Quang Loc <phongcachhiphop@gmail.com>
2017-09-09 22:01:20 +0700

    add event.prevendefault

--- src/mouse.ts
+++ src/mouse.ts
@@ -14,16 +14,19 @@ export let getMouseStatus = () => mouseStatus;
 
 window.onmousedown = (e: MouseEvent) => {
   mouseStatus = MouseStatus.DOWN;
+  e.preventDefault();
 };
 
 window.onmousemove = (e: MouseEvent) => {
   const t = celm.getBoundingClientRect();
   currentX = clamp((e.clientX - t.left) * scrwidth / celm.offsetWidth, 0, scrwidth);
   currentY = clamp((e.clientY - t.top) * scrheight / celm.offsetHeight, 0, scrheight);
+  e.preventDefault();
 };
 
 window.onmouseup = (e: MouseEvent) => {
   mouseStatus = MouseStatus.UP;
+  e.preventDefault();
 };
 
 export let getMousePos = () => [currentX, currentY];
